fb499520a9e2937aee07f4cfa08cf87ca8d0b99b
Lugana707 <sam.m.albon@outlook.com>
2021-09-23 22:12:46 -0400

    Added memory-cache to package endpoint

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -1,20 +1,39 @@
 import { RequestHandler } from 'express';
 import got from 'got';
 import * as semver from 'semver';
+import * as cache from 'memory-cache';
 import { INPMPackageVersion, IPackage } from './types';
 
-const getPackageFromNpmApi = async (name: string, version: string): Promise<IPackage> => {
+const CACHE_TIME_MILLISECONDS = 60 * 1000;
+
+const getPackageFromNpmApiOrCache = async (name: string, version: string): Promise<INPMPackageVersion> => {
+  const cacheKey = `${name}@${version}`;
+  const cachedValue = cache.get(cacheKey);
+  if (!!cachedValue) {
+    console.debug('Cache hit!', { cachedValue });
+
+    return cachedValue;
+  }
+
+  const npmPackageVersion: INPMPackageVersion = await got(`https://registry.npmjs.org/${name}/${version}`).json();
+
+  cache.put(cacheKey, npmPackageVersion, CACHE_TIME_MILLISECONDS);
+
+  return npmPackageVersion;
+};
+
+const getPackageAndDependencies = async (name: string, version: string): Promise<IPackage> => {
   const { version: minVersion } = semver.minVersion(version);
 
-  const npmPackage: INPMPackageVersion = await got(`https://registry.npmjs.org/${name}/${minVersion}`).json();
-
-  const { dependencies = {} } = npmPackage;
+  const { dependencies = {} } = await getPackageFromNpmApiOrCache(name, minVersion);
 
   const nestedDependencies = await Promise.all(
-    Object.keys(dependencies).map((packageName) => getPackageFromNpmApi(packageName, dependencies[packageName])),
+    Object.keys(dependencies).map((packageName) => getPackageAndDependencies(packageName, dependencies[packageName])),
   );
 
-  return { name, version, dependencies: nestedDependencies };
+  const npmPackageVersion: IPackage = { name, version, dependencies: nestedDependencies };
+
+  return npmPackageVersion;
 };
 
 /**
@@ -28,7 +47,7 @@ export const getPackage: RequestHandler = async function (req, res, next) {
   }
 
   try {
-    const result = await getPackageFromNpmApi(name, version);
+    const result = await getPackageAndDependencies(name, version);
 
     return res.status(200).json(result);
   } catch (error) {
