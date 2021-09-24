14dc774291b18e34d45be92f6b1a19b490be8a60
Lugana707 <sam.m.albon@outlook.com>
2021-09-23 21:15:17 -0400

    Added gzip compression, best practice for expressjs

--- exercises/npm-registry/src/app.ts
+++ exercises/npm-registry/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as compression from 'compression';
 import { getPackage } from './package';
 
 /**
@@ -7,6 +8,7 @@ import { getPackage } from './package';
 export function createApp() {
   const app = express();
 
+  app.use(compression());
   app.use(express.json());
 
   app.get('/package/:name/:version', getPackage);
